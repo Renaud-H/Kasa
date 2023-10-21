66f162ecb102dbed938e6cb72fa6a47323f075fb
Renaud-H <theqof@hotmail.fr>
2023-10-21 17:52:40 +0200

    navbar edit

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,22 +1,22 @@
 // import React from 'react'
-// import {Link, NavLink} from "react-router-dom"
+// import { NavLink } from 'react-router-dom';
 
-
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 
 // Import du css
-import "../styles/Navbar.css"
+import '../styles/Navbar.css';
 
 function Navbar() {
-    return ( 
-        <div>
-            <img src={logo} alt='Logo de la marque Kasa' /> 
-
+    return (
+        <div className="navbar">
+            <img src={logo} alt="Logo de la marque Kasa" />
+            <nav>
+                <a href="http://localhost:3000/">
+                    <ul>http://localhost:3000/</ul>
+                </a>
+            </nav>
         </div>
-
-    )
-
-
+    );
 }
 
-export default Navbar
+export default Navbar;
